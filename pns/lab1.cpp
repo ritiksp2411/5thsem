469b6eaace20b6f6425b4d5fad79d005890458bf
ritiksp2411 <ritik.rjt@gmail.com>
2020-08-07 11:57:11 +0530

    PnS commit

---
+++ pns/lab1.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	int r;
+	cin >> r;
+	if (r >= 75 && r <= 85) {
+		for (int j = 0; j <= r; j++) {
+			int xmax = sqrt(r * r - j * j);
+			for (int i = 0; i >= -1 * xmax; i--) {
+				cout << "(" << i << "," << j << ")" << ",";
+			}
+		}
+	}
+	else {
+		cout << "Give Integer between 75 to 85 as input";
+	}
+	return 0;
+}
