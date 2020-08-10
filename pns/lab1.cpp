b2ce39aee2e48febba6dea2eb54975e42ce6fa90
ritiksp2411 <ritik.rjt@gmail.com>
2020-08-10 14:43:28 +0530

    updated pns lab1 part2

--- pns/lab1.cpp
+++ pns/lab1.cpp
@@ -1,21 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main() {
-#ifdef _DEBUG
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
-#endif
 	int r;
 	cin >> r;
+	ofstream myfile;
+	myfile.open("output.txt");
 	if (r >= 75 && r <= 85) {
-		for (int j = 0; j <= r; j++) {
+		for (int j = 0; j < r; j++) {
 			int xmax = sqrt(r * r - j * j);
+			if(xmax*xmax==r * r - j * j) xmax--;
 			for (int i = 0; i >= -1 * xmax; i--) {
+				myfile << "(" << i << "," << j << ")" << ",";
 				cout << "(" << i << "," << j << ")" << ",";
 			}
 		}
 		cout << endl;
 	}
-	else cout << "Give Integer between 75 to 85 as input" << endl;
+	else{
+		myfile << "Give Integer between 75 to 85 as input(Invalid Input)" << endl;
+		cout << "Give Integer between 75 to 85 as input(Invalid Input)" << endl;
+	}
+	myfile.close();
 	return 0;
 }
