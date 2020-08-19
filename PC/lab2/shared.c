444e204187db80c9d8e714849756a8b1ffe7a8f9
ritiksp2411 <ritik.rjt@gmail.com>
2020-08-19 14:20:29 +0530

    Added the files for lab2_PC

---
+++ PC/lab2/shared.c
@@ -0,0 +1,11 @@
+#include<omp.h>
+#include<stdio.h>
+int main(){
+    int x=0;
+    #pragma omp parallel shared(x){
+        int tid=omp_get_thread_num();
+        x=x+1;
+        printf("Thread [%d] value of x is %d \n",tid,x);
+    }
+}
+//gcc prog1.c -fopenmp
