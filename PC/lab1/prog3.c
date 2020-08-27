068801d8a0e2d728d173b2964f96c5892b1b3794
ritiksp2411 <ritik.rjt@gmail.com>
2020-08-27 18:10:59 +0530

    Files for PC lab3

--- PC/lab1/prog3.c
+++ PC/lab1/prog3.c
@@ -1,8 +1,11 @@
 #include<omp.h>
 #include<stdio.h>
 int main(){
-    #pragma omp parallel num_threads(4)
+    int x=10;
+    #pragma omp parallel private(x) num_threads(4)
     {
+        x=x+1;
+        printf("%d\n",x);
         int tid=omp_get_thread_num();
         printf("Hello world from thread=%d\n",tid);
     }
